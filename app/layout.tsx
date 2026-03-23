f682184565c13f704b575bae1d2276b9e9613787
Vasiliy <vasa59017@mail.ru>
2026-03-23 21:21:15 +0300

    fixed build (middleware.ts > proxy.ts, delete headers in layout.tsx)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,8 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header"
-import HeaderWhiteLogo from "@/components/header-white-logo"
 import HeaderWrapper from "@/components/HeaderWrapper";
-
 import Footer from "@/components/footer"
-import CardNews from "@/components/news/card-news";
-import { headers } from 'next/headers';
 
 const roboto = Roboto({
   weight: ["100" , "200" , "300" , "400" , "500" , "600" , "700" , "800" , "900"],
@@ -19,10 +14,6 @@ export const metadata: Metadata = {
   title: "Купить квартиры в Великом Новгороде и области в новостройках от застройщика",
   description: "Новгородская строительная компания «Глория», действуя как генеральный подрядчик, на протяжении 20 лет имеет репутацию надежного делового застройщика ... квартиры Великий Новгород. строительство и продажа квартир. отдел продаж. +7(8162) 623-800.",
 };
-
-  const headersList = await headers();
-  const pathname = headersList.get('x-pathname') || '';
-  const isUnnatovPage = pathname === '/unnatov' || pathname === '/unnatov/';
 
 export default function RootLayout({
   children,
